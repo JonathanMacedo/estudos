8275ecd7ace95ea0eb3513ba18e236287e1a49d0
JonathanMacedo <148797702+JonathanMacedo@users.noreply.github.com>
2024-08-11 08:15:03 -0300

    estudos banco de dados

---
+++ banco_de_dados/mysql/aula03_aula04/aula03.sql
@@ -0,0 +1,18 @@
+
+/*
+create schema cfbcursos;
+create table cliente(
+   i_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   n_nomecliente_cliente varchar(50) NOT NULL,
+   s_cpfcliente_cliente varchar(11) NOT NULL,
+   d_nascimentocliente_cliente datetime);
+*/
+
+/*
+drop schema cfbcursos; Exclui a tabela com todos os dados dentro dele.
+
+drop table cliente; Exclui apenas a tabela especificada.
+
+Utilidade: se quiser rodar um codigo que está comentado, 
+selecione o trecho comentado e rode ele. 😁
+*/
