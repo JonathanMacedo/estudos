8935fa656e6064a86511418757cbfd0ddc0d0630
JonathanMacedo <148797702+JonathanMacedo@users.noreply.github.com>
2024-08-13 18:27:43 -0300

    13/08

---
+++ banco_de_dados/mysql/aula12/aula12.sql
@@ -0,0 +1,9 @@
+/* FROM é uma clausula do SELECT, outras clausulas:
+FROM: Comando usado para fazer consulta;
+WHERE: Comando de filtro das consultas;
+GROUP BY: Comando de agrupamento de linhas;
+HAVING: Comando de filtro de elementos não desejados;
+ORDER BY: Comando serve para indicar a ordem dos elementos.
+*/
+
+select upper(s_nomecliente_cliente) from cliente where i_cliente_cliente=1
