9eaf08da7b5a60ed6746b15a76dd58011fe6a88f
JonathanMacedo <148797702+JonathanMacedo@users.noreply.github.com>
2024-08-15 17:05:48 -0300

    15/08

--- banco_de_dados/mysql/aula26/aula26.sql
+++ banco_de_dados/mysql/aula26/aula26.sql
@@ -1,8 +1,4 @@
-/*
-SQL SERVER 
-SELECT TOP 5 * FROM cliente
-*/
+select * from venda v 
+inner join cliente c on c.i_cliente_cliente = v.i_cliente_cliente
+where f_valor_venda = (select max(f_valor_venda) from venda)
 
-select * from venda v
-inner  join cliente c on c.i_cliente_cliente = v.i_cliente_cliente
-order by f_valor_venda desc limit 3
