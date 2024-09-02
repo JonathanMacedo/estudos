0864f6a825f5c32944c072f5fc289f8149c41669
JonathanMacedo <148797702+JonathanMacedo@users.noreply.github.com>
2024-09-02 20:23:02 -0300

    02/09

---
+++ react/projeto1/src/pages/index.tsx
@@ -0,0 +1,17 @@
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
+
+const nome = "Jonathan";
+let canal = 'CFBCursos'
+
+export default function Home() {
+  return (
+
+      <div>
+        <p>{nome}</p>
+        <p>{canal}</p>
+      </div>
+ 
+  );
+}
